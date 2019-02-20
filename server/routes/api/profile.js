85cf29d181988426d16b2d05a5a8763a23c082e9
Fayvor George <phavorsparks@gmail.com>
2019-02-21 00:29:14 +0100

    update comments

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -5,8 +5,8 @@ const profileController = require('../../controllers/profile')
 
 // Build the routes here
 
-// @Route     /api/users/
-// @desc      Get all Users
+// @Route     /api/profile/
+// @desc      Get user's profile
 // @access    Public
 router.get('/', profileController.getProfile)
 
