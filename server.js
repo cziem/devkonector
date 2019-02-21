53d4e1349f93aac59938bb17b688fd4175a945a6
Fayvor George <phavorsparks@gmail.com>
2019-02-21 13:59:42 +0100

    setup passport in the server

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-require('./server/config/config')
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+require("./server/config/config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-const port = process.env.PORT
-const uri = process.env.MONGODB_URI
+const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
 
-const users = require('./server/routes/api/users')
-const profile = require('./server/routes/api/profile')
-const posts = require('./server/routes/api/posts')
+const users = require("./server/routes/api/users");
+const profile = require("./server/routes/api/profile");
+const posts = require("./server/routes/api/posts");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.locals.moment = require('moment')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.locals.moment = require("moment");
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
-  .then(() => console.log('connected to DB...'))
-  .catch((err) => console.log('Failed to connect to DB...', err))
+// Passport Middleware & configuration
+app.use(passport.initialize());
+require("./server/config/passport")(passport);
 
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("connected to DB..."))
+  .catch(err => console.log("Failed to connect to DB...", err));
 
-app.listen(port, () => console.log(`now-fashion running on http://localhost:${port}`))
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.listen(port, () =>
+  console.log(`devkonector running on http://localhost:${port}`)
+);
