6d01ef31a5e01f1ee7edead66422967142ed51ee
Fayvor George <phavorsparks@gmail.com>
2019-02-21 13:59:32 +0100

    Add passport configuration

---
+++ server/config/passport.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const secret = process.env.USER_SECRET;
+
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = secret;
+
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        let user = await User.findById(jwt_payload.id);
+
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      } catch (error) {
+        return done(err, false);
+      }
+    })
+  );
+};
