06c86d2fc32441e6dfbaba91314e95f6ca3c736b
Fayvor George <phavorsparks@gmail.com>
2019-02-22 13:43:33 +0100

    UPDATE desc

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -6,21 +6,25 @@ const userController = require("../../controllers/users");
 
 // Build the routes here
 
+// @type      GET
 // @Route     /api/users/
 // @desc      Get all Users
 // @access    Public
 router.get("/", userController.allUsers);
 
+// @type      POST
 // @Route     /api/users/register
 // @desc      Register a new user
 // @access    Public
 router.post("/register", userController.register);
 
+// @type      POST
 // @Route     /api/users/login
 // @desc      Login a user / Returning a Token
 // @access    Public
 router.post("/login", userController.login);
 
+// @type      GET
 // @Route     /api/users/current
 // @desc      Return a current user
 // @access    Private
