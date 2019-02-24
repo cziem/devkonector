3c71bee6ef4cba0b3070a07a047f5dc09103655c
Fayvor George <phavorsparks@gmail.com>
2019-02-24 17:26:29 +0100

    Add the auth Components: register and login

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Register</h2>
+      </div>
+    );
+  }
+}
+
+export default Register;
