acc88e7580fe46133f75def9e245bbe4b44efd83
Fayvor George <phavorsparks@gmail.com>
2019-02-24 16:50:46 +0100

    setup Landing Page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "../styles/App.css";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
+import Landing from "./layouts/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1>DevKonector</h1>
+        <Landing />
         <Footer />
       </div>
     );
