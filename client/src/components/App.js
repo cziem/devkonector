d4efa011f089ccb1ab3f4bb9fd4ad0647819a334
Fayvor George <phavorsparks@gmail.com>
2019-02-24 16:36:54 +0100

    Add Footer component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <h1>DevKonector</h1>
+        <Footer />
       </div>
     );
   }
