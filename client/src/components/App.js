4dddc62a4718fa420022c6eb609e15cf39d16ed0
Fayvor George <phavorsparks@gmail.com>
2019-02-24 17:26:01 +0100

    Adjust the navbar to link to the right links: register and login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../styles/App.css";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import Landing from "./layouts/Landing";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -28,12 +28,12 @@ class Navbar extends Component {
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
+                <a className="nav-link" href="/register">
                   Sign Up
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <a className="nav-link" href="/login">
                   Login
                 </a>
               </li>
