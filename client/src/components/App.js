c2d6ee2dc3e28fe2c24d7bbb16353b3119c6b2d8
Fayvor George <phavorsparks@gmail.com>
2019-02-25 13:20:45 +0100

    logout user when token expires

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -26,15 +26,15 @@ if (localStorage.jwtToken) {
   store.dispatch(setCurrentUser(decoded));
 
   // Check for expired token
-  const currentTime = Date.now() / 1000
+  const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     // Logout user
-    store.dispatch(logoutUser())
+    store.dispatch(logoutUser());
 
     // TODO: Clear current profile
 
     // Redirect to Login
-    window.location.href = '/login'
+    window.location.href = "/login";
   }
 }
 
