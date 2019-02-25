5c9169e80d3384e4b2fecd4d4f647f259dcadf82
Fayvor George <phavorsparks@gmail.com>
2019-02-25 11:17:48 +0100

    persist logged in user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwtDecode from "jwt-decode";
+
 import store from "../store/store";
+import setAuthToken from "../utils/setAuthToken";
+import { setCurrentUser } from "../actions/authActions";
+
 import "../styles/App.css";
 import Navbar from "./layouts/Navbar";
 import Footer from "./layouts/Footer";
 import Landing from "./layouts/Landing";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // Decode token and get user info and exp
+  const decoded = jwtDecode(localStorage.jwtToken);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
