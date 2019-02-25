4f0966bb387927357db5c5dd858182c34c7a1171
Fayvor George <phavorsparks@gmail.com>
2019-02-25 12:55:40 +0100

    logout a user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -50,3 +50,18 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem("jwtToken");
+
+  // Remove auth header for future requests
+  setAuthToken(false);
+
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+
+  // Redirect to the homepage
+  window.location.href = "/";
+};
