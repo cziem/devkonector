0a096a1df20c9460a7e48753cdcc5cc9c86419f2
Fayvor George <phavorsparks@gmail.com>
2019-03-04 17:44:30 +0100

    Add spinner to repoItems while app is fetching data

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import Spinner from "../helpers/Spinner";
+
 class ProfileGithub extends Component {
   state = {
     clientId: process.env.REACT_APP_CLIENT_ID,
@@ -30,36 +32,43 @@ class ProfileGithub extends Component {
 
   render() {
     const { repos } = this.state;
-    const repoItems = repos.map(repo => (
-      <div key={repo.id} className="card card-body mb-2">
-        <div className="row">
-          <div className="col-md-6">
-            <h4>
-              <a
-                href={repo.html_url}
-                rel="noopener noreferrer"
-                className="text-info"
-                target="_blank"
-              >
-                {repo.name}
-              </a>
-            </h4>
-            <p>{repo.description}</p>
-          </div>
-          <div className="col-md-6">
-            <span className="badge badge-info mr-1">
-              Stars: {repo.stargazers_count}
-            </span>
-            <span className="badge badge-secondary mr-1">
-              Watchers: {repo.watchers_count}
-            </span>
-            <span className="badge badge-success">
-              Forks: {repo.forks_count}
-            </span>
+    let repoItems;
+
+    if (repos.length > 0) {
+      repoItems = repos.map(repo => (
+        <div key={repo.id} className="card card-body mb-2">
+          <div className="row">
+            <div className="col-md-6">
+              <h4>
+                <a
+                  href={repo.html_url}
+                  rel="noopener noreferrer"
+                  className="text-info"
+                  target="_blank"
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div className="col-md-6">
+              <span className="badge badge-info mr-1">
+                Stars: {repo.stargazers_count}
+              </span>
+              <span className="badge badge-secondary mr-1">
+                Watchers: {repo.watchers_count}
+              </span>
+              <span className="badge badge-success">
+                Forks: {repo.forks_count}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ));
+    } else {
+      repoItems = <Spinner />;
+    }
+
     return (
       <div ref="myRef">
         <hr />
