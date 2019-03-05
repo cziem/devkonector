a4cc9d5ec22645a88a5bf5c82590fb48eb8a493c
Fayvor George <phavorsparks@gmail.com>
2019-03-05 21:57:31 +0100

    setup server for build and deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const path = require("path");
 
 const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
@@ -33,6 +34,16 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () =>
   console.log(`devkonector running on http://localhost:${port}`)
