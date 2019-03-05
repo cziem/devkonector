82b19152d0cd80d83b740c1271cdeb6e68a6a8da
Fayvor George <phavorsparks@gmail.com>
2019-03-05 21:09:42 +0100

    remove console..log call

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -12,7 +12,6 @@ class CommentItem extends Component {
 
   render() {
     const { comment, postId, auth } = this.props;
-    console.log(this.props);
     return (
       <div className="card card-body mb-3">
         <div className="row">
